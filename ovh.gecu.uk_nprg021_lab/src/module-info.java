762ad5a61c560ce9a67c303e827b26f68acbc89b
Guillaume Chauveau <guillaumejchauveau.ops.gecu@outlook.com>
2020-05-17 00:19:28 +0200

    fix

--- ovh.gecu.uk_nprg021_lab/src/module-info.java
+++ ovh.gecu.uk_nprg021_lab/src/module-info.java
@@ -4,9 +4,9 @@ import ovh.gecu.uk_nprg021_lab.p1.MyTextProc;
 import ovh.gecu.uk_nprg021_lab.p1.TextProcessor;
 
 module ovh.gecu.uk_nprg021_lab {
-  requires jdom;
   requires java.xml;
   requires java.sql;
+  requires jdom2;
   provides TextProcessor with MyTextProc;
   uses TextProcessor;
   provides MyPluginInterface with MyPlugin;
